2f04e762f77f65949066a3904bfb61899020157d
qnxy <qnxy1997@gmail.com>
2024-08-20 02:46:04 +0800

    增加 加解密处理接口/加解密处理器标注注解/支持的加密类型枚举

--- src/main/java/com/qnxy/blog/core/annotations/Sensitive.java
+++ src/main/java/com/qnxy/blog/core/annotations/Sensitive.java
@@ -16,12 +16,55 @@ public @interface Sensitive {
      * 指定该字段加密的方式
      * 默认为 AES 加密
      */
-    CryptoType cryptoType() default CryptoType.AES;
+    EncryptType value() default EncryptType.AES;
 
-    enum CryptoType {
+
+    /**
+     * 支持的加密类型
+     */
+    enum EncryptType {
         SM4,
         AES,
     }
 
+
+    /**
+     * 加解密处理器
+     * 标注在密文处理器上 {@link CiphertextHandler}, 并指定该处理器处理的加密方式
+     */
+    @Documented
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @interface Handler {
+
+        /**
+         * 当前加解密处理器
+         */
+        EncryptType value();
+
+    }
+
+    /**
+     * 加解密处理接口
+     */
+    interface CiphertextHandler {
+
+        /**
+         * 加密
+         *
+         * @param plainText 待加密数据
+         * @return 密文
+         */
+        String encrypt(String plainText);
+
+        /**
+         * 解密
+         *
+         * @param ciphertext 密文
+         * @return 解密后的数据
+         */
+        String decrypt(String ciphertext);
+
+    }
 
 }
