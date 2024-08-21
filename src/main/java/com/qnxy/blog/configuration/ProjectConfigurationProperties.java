3bdf04dd5699c0a6f2ddd1707b957f7c03af7f2f
qnxy <qnxy1997@gmail.com>
2024-08-21 08:43:42 +0800

    增加AES/SM4加密密码配置

--- src/main/java/com/qnxy/blog/configuration/ProjectConfigurationProperties.java
+++ src/main/java/com/qnxy/blog/configuration/ProjectConfigurationProperties.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 项目属性配置信息
- * 
+ *
  * @author Qnxy
  */
 @Data
@@ -18,5 +18,17 @@ public class ProjectConfigurationProperties {
      * 文件服务器地址
      */
     private String fileUploadServerBaseUrl;
+
+    /**
+     * 敏感数据 AES 加密密钥
+     * 秘钥可以为 BASE64 或 HEX 编码类型
+     */
+    private String sensitiveAesKey;
+
+    /**
+     * 敏感数据 SM4 加密密钥
+     * 秘钥可以为 BASE64 或 HEX 编码类型
+     */
+    private String sensitiveSm4Key;
 
 }
